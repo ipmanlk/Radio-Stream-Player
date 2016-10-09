a7d6ee563557d7d4c1a9be4e8af31b6ca3bb1ca0
ipman_98 <ipman_98@yahoo.com>
2016-10-09 18:13:11 +0530

    Initial commit

---
+++ stations.js
@@ -0,0 +1,161 @@
+<script>
+function FMDerana(){
+
+var player=document.getElementById('player');
+var sourceMp3=document.getElementById('player');
+
+sourceMp3.src='http://109.123.114.30:7130/;';
+
+player.load(); //just start buffering (preload)
+player.play(); //start playing
+
+
+}
+
+function GoldFM(){
+
+var player=document.getElementById('player');
+var sourceMp3=document.getElementById('player');
+
+sourceMp3.src='http://209.95.39.46:8020/;stream.nsv';
+
+player.load();
+player.play();
+}
+
+function HeartFM(){
+
+var player=document.getElementById('player');
+var sourceMp3=document.getElementById('player');
+
+sourceMp3.src='http://198.100.148.141:8054/;.mp3';
+
+player.load();
+player.play();
+
+function HiruFM(){
+
+var player=document.getElementById('player');
+var sourceMp3=document.getElementById('player');
+
+sourceMp3.src='http://209.95.39.46:8010/;';
+
+player.load();
+player.play();
+}
+
+function KirulaFM(){
+
+var player=document.getElementById('player');
+var sourceMp3=document.getElementById('player');
+
+sourceMp3.src='http://206.190.130.180:8044/;';
+
+player.load();
+player.play();
+}
+
+function NethFM(){
+
+var player=document.getElementById('player');
+var sourceMp3=document.getElementById('player');
+
+sourceMp3.src='http://66.55.144.67:8104/;';
+
+player.load();
+player.play();
+}
+
+
+function RhythmFM(){
+
+var player=document.getElementById('player');
+var sourceMp3=document.getElementById('player');
+
+sourceMp3.src='http://192.99.99.195:8030/';
+
+player.load();
+player.play();
+}
+
+
+function ShaaFM(){
+
+var player=document.getElementById('player');
+var sourceMp3=document.getElementById('player');
+
+sourceMp3.src='http://37.130.229.188:8015/;';
+
+player.load();
+player.play();
+}
+
+function ShreeFM(){
+
+var player=document.getElementById('player');
+var sourceMp3=document.getElementById('player');
+
+sourceMp3.src='http://live2.shree.fm/;.mp3';
+
+player.load();
+player.play(); //start playing
+}
+
+
+function SirasaFM(){
+
+var player=document.getElementById('player');
+var sourceMp3=document.getElementById('player');
+
+sourceMp3.src='http://76.164.217.100:7024/;';
+
+player.load();
+player.play();
+}
+
+
+function SiyathaFM(){
+
+var player=document.getElementById('player');
+var sourceMp3=document.getElementById('player');
+
+sourceMp3.src='http://s3.voscast.com:8408/;';
+
+player.load();
+player.play();
+
+
+function SunFM(){
+
+var player=document.getElementById('player');
+var sourceMp3=document.getElementById('player');
+
+sourceMp3.src='http://37.130.229.188:8090/;';
+
+player.load();
+player.play();
+}
+
+
+function VFM(){
+
+var player=document.getElementById('player');
+var sourceMp3=document.getElementById('player');
+
+sourceMp3.src='http://85.25.141.13:8080/;stream.mp3';
+
+player.load();
+player.play();
+}
+
+function YFM(){
+
+var player=document.getElementById('player');
+var sourceMp3=document.getElementById('player');
+
+sourceMp3.src='http://76.164.217.100:7036/;';
+
+player.load();
+player.play();
+}
+</script>
